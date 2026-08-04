392fc92f2892292c1b6dec53bef3e2bbd0824ab9
fobong58-netizen <fobong58@gmail.com>
2026-08-04 06:51:11 +0100

    changed my js

--- game.js
+++ game.js
@@ -7,6 +7,15 @@ const inputNumEl = document.querySelector(`.input-number`);
 const highScoreEl = document.querySelector(`.high-score`);
 const scoreEl = document.querySelector(`.score`);
 
+const mobileContainerEl = document.querySelector(`.mobile-container`);
+const mobileBtnPlayEl = document.querySelector(`.mobile-btn-again`);
+const mobileBtnChckEl = document.querySelector(`.mobile-btn-again3`);
+const mobileHideNumEl = document.querySelector(`.mobile-hide-num`);
+const mobileMsgEl = document.querySelector(`.mobile-message`);
+const mobileInputNumEl = document.querySelector(`.mobile-input-number`);
+const mobileHighScoreEl = document.querySelector(`.mobile-high-score`);
+const mobileScoreEl = document.querySelector(`.mobile-score`);
+
 //generate a number 1 to 20
 
 let scretNum = Math.trunc(Math.random() * 20 + 1);
@@ -81,5 +90,72 @@ btnPlayEl.addEventListener(`click`,()=>{
 console.log(scretNum);
 
 
+//mobile starts
 
 
+
+
+
+
+
+mobileBtnChckEl.addEventListener(`click`,()=>{
+ const guess = Number(mobileInputNumEl.value);
+
+
+ //check empty input
+ if(guess){
+
+    //not match hide number 
+
+    if(guess != scretNum){
+
+        if(score > 1){
+
+            score--;
+            mobileScoreEl.textContent = score;
+
+            mobileMsgEl.textContent = guess > scretNum ? "Too High" : "Too Low";
+            mobileScoreEl.textContent = score;
+
+        }else{
+            mobileMsgEl.textContent = "you are a big foolish loser"
+            mobileContainerEl.style.backgroundColor = "pink";
+            mobileScoreEl.textContent = 0
+        }
+
+    }else{//succes
+       mobileHideNumEl.textContent = scretNum;
+       mobileHideNumEl.style.width = "50%";
+       mobileHideNumEl.style.transition = "all 0.5s ease-in";
+       mobileContainerEl.style.backgroundColor = "purple";
+       mobileBtnPlayEl.style.backgroundColor = "yellow";
+       mobileMsgEl.textContent = "Congratulations you have won!!";
+    }
+
+ }else{
+    mobileMsgEl.textContent = "pls choose a number"
+ }
+});
+
+
+//display message
+
+const displayMobileMessage = function (message){
+    mobileMsgEl.textContent = message;
+};
+
+//reset the game 
+
+mobileBtnPlayEl.addEventListener(`click`,()=>{
+    score = 20;
+    scretNum = Math.floor(Math.random()*20)+1;
+    mobileScoreEl.textContent = score;
+    mobileHideNumEl.textContent = "?";
+    mobileHideNumEl.style.width = "25%";
+    mobileHideNumEl.style.transition = "all 0.5s ease-in";
+    mobileInputNumEl.value = ""
+    mobileContainerEl.style.backgroundColor = "pink";
+    displayMobileMessage("try again HeGoat.....")
+})
+
+console.log(scretNum);
